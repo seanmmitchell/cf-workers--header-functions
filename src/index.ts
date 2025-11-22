6ab454bc9af59deab3126bd4c952d1b1444c9ebc
Sean Mitchell <smitchell@srnd.net>
2025-11-22 02:44:42 -0500

    Inital Commit!

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,16 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		let url = new URL(request.url)
+
+		if (url.pathname == "/") {
+			let responseText = ""
+			request.headers.forEach((hVal, hKey) => {
+				responseText += `${hKey}: ${hVal}\r\n`
+			});
+			return new Response(responseText)
+		} else {
+			return new Response("404")
+		}
 	},
 } satisfies ExportedHandler<Env>;
